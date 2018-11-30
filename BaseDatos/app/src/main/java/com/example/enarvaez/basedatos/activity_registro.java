569d7395f36a085f6dd967845d82118ae89ddddd
ENOC NARVAEZ <enocnarvaez@unan.edu.ni>
2018-11-29 21:39:29 -0600

    metodos_de_autenticacion

--- BaseDatos/app/src/main/java/com/example/enarvaez/basedatos/activity_registro.java
+++ BaseDatos/app/src/main/java/com/example/enarvaez/basedatos/activity_registro.java
@@ -1,10 +1,13 @@
 package com.example.enarvaez.basedatos;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -15,7 +18,9 @@ public class activity_registro extends AppCompatActivity {
     private EditText TextEmail;
     private EditText TextPassword;
     private Button btnRegistrar;
-    private ProgressBar progresoDialogo;
+
+
+    private ProgressDialog ProgressDialog;
 
     private FirebaseAuth firebaseAuth;
     @Override
@@ -28,11 +33,30 @@ public class activity_registro extends AppCompatActivity {
         TextEmail=(EditText) findViewById(R.id.txtcorreo);
         TextPassword= (EditText) findViewById(R.id.txtcorreo);
         btnRegistrar=(Button)findViewById(R.id.btnRegistro);
-        //progresoDialogo= new progresoDialogo(this);
+        ProgressDialog= new ProgressDialog(this);
 
+        btnRegistrar.setOnClickListener(this);
 
+    }
 
+    private void registrar_usuario()
+    {
 
+        //obtenemos el email de la caja de texto
+        String email=TextEmail.getText().toString().trim();
+        String pass=TextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+
+            Toast.makeText(this,"se debe ingresar un email valido",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(TextUtils.isEmpty(pass))
+        {
+
+            Toast.makeText(this,"Se debe ingresar una contraseña",Toast.LENGTH_LONG).show();
+            return;
+        }
 
     }
 }
