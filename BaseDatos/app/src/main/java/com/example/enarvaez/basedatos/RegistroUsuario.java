b33d3729b2d86166520dee7b2f1ff09614689d88
ENOC NARVAEZ <enocnarvaez@unan.edu.ni>
2018-11-30 04:21:15 -0600

    registro_actualizacion

--- BaseDatos/app/src/main/java/com/example/enarvaez/basedatos/RegistroUsuario.java
+++ BaseDatos/app/src/main/java/com/example/enarvaez/basedatos/RegistroUsuario.java
@@ -1,4 +1,6 @@
 package com.example.enarvaez.basedatos;
 
-public class RegistroUsuario {
+import android.support.v7.app.AppCompatActivity;
+
+public class RegistroUsuario extends AppCompatActivity {
 }
