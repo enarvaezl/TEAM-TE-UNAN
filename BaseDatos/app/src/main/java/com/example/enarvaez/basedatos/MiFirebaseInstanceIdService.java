14d2275aa4126f80c175c9ea912e780020d9f412
ENOC NARVAEZ <enocnarvaez@unan.edu.ni>
2018-11-30 10:01:58 -0600

    Ingresando_clases_de_mensaje

---
+++ BaseDatos/app/src/main/java/com/example/enarvaez/basedatos/MiFirebaseInstanceIdService.java
@@ -0,0 +1,32 @@
+package com.example.enarvaez.basedatos;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MiFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    public static final String TAG;
+
+    static {
+        TAG = "NOTICIAS";
+    }
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+
+
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+        Log.d(TAG, "Token: " + token);
+
+        enviarTokenAlServidor(token);
+    }
+
+    private void enviarTokenAlServidor(String token) {
+        // Enviar token al servidor
+    }
+}
+
---
+++ BaseDatos/app/src/main/java/com/example/enarvaez/basedatos/MiFirebaseMessagingService.java
@@ -0,0 +1,34 @@
+package com.example.enarvaez.basedatos;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MiFirebaseMessagingService extends FirebaseMessagingService {
+
+
+
+    public static final String TAG = "NOTICIAS";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        String from = remoteMessage.getFrom();
+        Log.d(TAG, "Mensaje recibido de: " + from);
+
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Notificación: " + remoteMessage.getNotification().getBody());
+
+            mostrarNotificacion(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }
+
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Data: " + remoteMessage.getData());
+        }
+
+    }
+
+}
+
