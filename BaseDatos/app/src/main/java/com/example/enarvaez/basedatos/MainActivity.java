1890333d9783fd3874722cfef84537dd2d96b928
ENOC NARVAEZ <enocnarvaez@unan.edu.ni>
2018-11-29 19:12:42 -0600

    CREACION_METODOS

--- BaseDatos/app/src/main/java/com/example/enarvaez/basedatos/MainActivity.java
+++ BaseDatos/app/src/main/java/com/example/enarvaez/basedatos/MainActivity.java
@@ -1,40 +1,30 @@
 package com.example.enarvaez.basedatos;
-
 import android.support.annotation.NonNull;
-import android.support.constraint.solver.widgets.Snapshot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
-
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DataSnapshot;
 import  com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity  {
     DatabaseReference datos;
     Button mButtonSubirDatosFirebase;
     EditText mEditTextDatoNombreUsuario,mEditTextDatoApellidoUsuario,
         mEditTextDatoTelefonoUsuario, mEditTextDatoDireccionUsuario;
 
 
-       DatabaseReference Clases;
-
-        EditText txttema;
-        Spinner spinarea, spinseccion;
-        Button btnregistrar;
-
         @Override
             protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
+            super.onCreate(savedInstanceState);
 
-                setContentView(R.layout.activity_main);
+            setContentView(R.layout.activity_main);
 
             mButtonSubirDatosFirebase = findViewById(R.id.btnSubirDatos);
             mButtonSubirDatosFirebase.setOnClickListener(this);
@@ -43,47 +33,74 @@ public class MainActivity extends AppCompatActivity {
             mEditTextDatoTelefonoUsuario = findViewById(R.id.etTelefono);
             mEditTextDatoDireccionUsuario = findViewById(R.id.etDireccion);
 
-            datos= FirebaseDatabase.getInstance().getReference();
-        //
+            datos = FirebaseDatabase.getInstance().getReference();
+            solictarDatosFirebase();
+        }
+            //Metodo que solicta los datos
+            private void solictarDatosFirebase()
+            {
+                datos.child("Usuario").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        for (final DataSnapshot snapshot : dataSnapshot.getChildren()) {
 
 
-           private void solictarDatosFirebase()
+                            datos.child("Usuario").child(snapshot.getKey()).addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    Clases user = snapshot.getValue(com.example.enarvaez.basedatos.Clases.class);
+                                    String nombre = user.getNombre();
+                                    String apellido = user.getApellido();
+                                    int telefono = user.getTelefono();
+                                    String direccion = user.getDireccion();
 
-        {
-          datos.child("Usuario").addValueEventListener(new ValueEventListener() {
-              @Override
-              public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                  for (final DataSnapshot snapshot: dataSnapshot.getChildren()) {
-
-
-                      datos.child("Usuario").child(snapshot.getKey()).addValueEventListener(new ValueEventListener() {
-                          @Override
-                          public void onDataChange(DataSnapshot dataSnapshot) {
-                              Clases user = snapshot.getValue(com.example.enarvaez.basedatos.Clases.class);
-                              String nombre= user.getNombre();
-                              String apellido= user.getApellido();
-                              int telefono=user.getTelefono();
-                              String direccion=user.getDireccion();
-
-                              long.e("nombre de Usuario");
-                          }
-
-                          @Override
-                          public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                          }
-                      });
+                                    Log.e("nombre de Usuario:" + "," + nombre);
+                                    Log.e("Apellido :" + "," + apellido);
+                                    Log.e("telefono :" + "," + telefono);
+                                    Log.e("direccion :" + "," + direccion);
+                                    Log.e("Datos: ", "" + snapshot.getValue());
 
 
-                  }
-              }
+                                }
 
-              @Override
-              public void onCancelled(@NonNull DatabaseError databaseError) {
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
 
-              }
-          });
+                                }
+                            });
+
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+
+
+            //metodo aue carga los datos
+
+            private void CargarDatos (String nombre, String apellido, int telefono, String direccion){
+
+                Map<String,Object> datosUsuarios= new HashMap<>();
+                datosUsuarios.put("nombre",nombre);
+                datosUsuarios.put("apellido", apellido);
+                datosUsuarios.put("telefono",telefono);
+                datosUsuarios.put("direccion",direccion);
+                datos.child("Usuario").push().setValue(datosUsuarios);
+
+            }
+
+
+
+
+
+
 
         }
 
@@ -93,5 +110,3 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-    }
